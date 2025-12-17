d1e1d5a417f9fafe4f1c2a85ab0e9eacb2ef275c
aleasistemi <aleasistemi.corsi@gmail.com>
2025-12-17 16:34:03 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 'base' deve essere './' o stringa vuota per permettere l'apertura 
-  // dei file direttamente dal filesystem di Android/iOS (file://)
+  // IMPORTANTE: base deve essere './' per funzionare su Android
   base: './',
   build: {
     outDir: 'dist',
