9b950f66fb5304c3b7bc04e077e45ab6a7c3d2ca
aleasistemi <aleasistemi.corsi@gmail.com>
2025-12-19 10:03:33 +0100

    Add files via upload

--- appConfig.ts
+++ appConfig.ts
@@ -1,6 +1,6 @@
 
 export const APP_CONFIG = {
-  // Imposta su 'MOBILE_TOTEM' per l'APK destinato allo smartphone dei mezzi.
-  // Imposta su 'STANDARD' per l'app completa gestionale/dashboard.
-  MODE: 'MOBILE_TOTEM' as 'STANDARD' | 'STANDARD'
+  // Imposta su 'MOBILE_TOTEM' solo per generare l'APK dello smartphone mezzi.
+  // Imposta su 'STANDARD' per l'app completa (PC, Dashboard, Amministrazione).
+  MODE: 'STANDARD' as 'STANDARD' | 'MOBILE_TOTEM'
 };
